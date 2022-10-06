5fa42957b9734b099283750a8b2c8f018ead2ebd
Gulshankhandale <gulshankhandale.btech@gmail.com>
2022-10-06 19:25:08 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import TopNavbar from './Components/TopNavbar';
+import HeroSection from './Components/HeroSection';
 
 function App() {
   return <>
 
-  <div>Hello</div>
+  <TopNavbar/>
+  <HeroSection/>
   
   </>
 }
